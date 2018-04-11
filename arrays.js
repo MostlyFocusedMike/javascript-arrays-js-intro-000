3ebcccce415ab01ef68c9a367b6afb6fba4384fe
Mike C <mikecronin92@gmail.com>
2018-04-11 05:04:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.unshift(element)
   return array
 }
